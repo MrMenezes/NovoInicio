2e736d3d1ba9f2e25c3e9042cd5b8c80cb310442
Erick <sr.tama@outlook.com>
2015-06-03 01:10:59 -0300

    Implement Tail e TailRandomize

--- app/src/main/java/com/mrmenezes/novoinicio/TailRandomize.java
+++ app/src/main/java/com/mrmenezes/novoinicio/TailRandomize.java
@@ -1,6 +1,7 @@
 package com.mrmenezes.novoinicio;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Mr Menezes on 02/06/2015.
@@ -9,17 +10,22 @@ public class TailRandomize {
     private ArrayList<Tail> listaTail = new ArrayList<Tail>();
 
     public TailRandomize(){
-        int[][] list = {{16, 88, 24, 48},{16, 88, 24, 48},{16, 88, 24, 48},{16, 88, 24, 48},{16, 88, 24, 48},{16, 88, 24, 48}};
+        int[][] list = {{16, 88, 24, 48},{8, 24, 88, 56},{16, 88, 80, 96},{40, 64, 96, 24},{16, 88, 24, 48},{16, 88, 24, 48}};
         int[][] matriz = {{0, 0, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {1, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 0}, {0, 0, 0, 0, 0, 0}};
         listaTail.add(new Tail(matriz,list));
     }
 
     public Tail getRMatriz(){
-        return this.listaTail.get(listaTail.size()-1);
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(listaTail.size());
+        return this.listaTail.get(randomInt);
     }
 
     public int[] getRList(Tail tail){
-        int[] lista = this.listaTail.get(this.listaTail.indexOf(tail)).getPeças()[1];
+
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(6);
+        int[] lista = this.listaTail.get(this.listaTail.indexOf(tail)).getPeças()[randomInt];
         return lista;
     }
 }
